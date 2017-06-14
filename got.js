7f3f36cfb981d72fbada53f684f03ad589c0ee47
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-14 03:44:27 +0000

    add android headers

--- got.js
+++ got.js
@@ -1,6 +1,10 @@
 const got = require('got');
 
-got('localhost:8888/server.php')
+got('localhost:8888/server.php', {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Mobile Safari/537.36'
+    }
+})
     .then(response => {
         console.log(response.body);
         //=> '<!doctype html> ...'
